d836334b31b41a84a50c902e2852364e077cf56e
Guilherme Tupinamba Duraes <guilherme.tupinamba@gmail.com>
2021-02-17 19:18:27 -0300

    Add: carrousel auto switch

--- assets/scripts/caroussel.js
+++ assets/scripts/caroussel.js
@@ -9,23 +9,59 @@ let slides = [
   document.getElementById('thirdSlide'),
 ];
 
+let time = 0,
+  newSlider = 0,
+  actualSlider = 0;
+
+let carrouselInterval = setInterval(() => {
+  if (actualSlider === newSlider) {
+    actualSlider < 2 ? ++actualSlider : (actualSlider = 0);
+    newSlider = actualSlider;
+    changeCarrousel();
+    carrouselInterval;
+  } else {
+    actualSlider = newSlider;
+    changeCarrousel();
+    carrouselInterval;
+  }
+}, 4000);
+
+carrouselInterval;
+
 pointers.forEach((pointer, index) => {
   pointer.onclick = () => {
-    let deactivatedPointers = pointers.filter(
-      (notPointer) => notPointer !== pointer
-    );
-    deactivatedPointers.forEach((deactivatedPointer) => {
-      if (deactivatedPointer.classList.contains('active')) {
-        deactivatedPointer.classList.remove('active');
-      }
-    });
-    if (!pointer.classList.contains('active')) {
-      pointer.classList.add('active');
+    newSlider = index;
+    time = 0;
+    if (actualSlider === newSlider) {
+      actualSlider < 2 ? ++actualSlider : (actualSlider = 0);
+      newSlider = actualSlider;
+      changeCarrousel();
+      carrouselInterval;
+    } else {
+      actualSlider = newSlider;
+      changeCarrousel();
+      carrouselInterval;
     }
-    slides.forEach((slide, i) => {
-      index === i
-        ? (slide.style.display = 'block')
-        : (slide.style.display = 'none');
-    });
   };
 });
+
+setInterval;
+
+function changeCarrousel() {
+  let deactivatedPointers = pointers.filter(
+    (notPointer) => notPointer !== pointers[actualSlider]
+  );
+  deactivatedPointers.forEach((deactivatedPointer) => {
+    if (deactivatedPointer.classList.contains('active')) {
+      deactivatedPointer.classList.remove('active');
+    }
+  });
+  if (!pointers[actualSlider].classList.contains('active')) {
+    pointers[actualSlider].classList.add('active');
+  }
+  slides.forEach((slide, i) => {
+    newSlider === i
+      ? (slide.style.display = 'block')
+      : (slide.style.display = 'none');
+  });
+}
