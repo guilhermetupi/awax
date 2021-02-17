b7eb2db0af6d0d39c2eda4ffca493d0f6e39329f
Guilherme Tupinamba Duraes <guilherme.tupinamba@gmail.com>
2021-02-17 03:00:18 -0300

    Add: Header, Banner, About us, Services

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Awax
-Project developed using Alex Bukin's [design](https://www.behance.net/gallery/58301549/Free-Landing-Page "Free Landing Page") using HTML5 and CSS3.
+
+Project developed based in Alex Bukin's [design](https://www.behance.net/gallery/58301549/Free-Landing-Page 'Free Landing Page') using HTML5 and CSS3.
---
+++ assets/scripts/caroussel.js
@@ -0,0 +1,31 @@
+let pointers = [
+  document.getElementById('first'),
+  document.getElementById('second'),
+  document.getElementById('third'),
+];
+let slides = [
+  document.getElementById('firstSlide'),
+  document.getElementById('secondSlide'),
+  document.getElementById('thirdSlide'),
+];
+
+pointers.forEach((pointer, index) => {
+  pointer.onclick = () => {
+    let deactivatedPointers = pointers.filter(
+      (notPointer) => notPointer != pointer
+    );
+    deactivatedPointers.forEach((deactivatedPointer) => {
+      if (deactivatedPointer.classList.contains('active')) {
+        deactivatedPointer.classList.remove('active');
+      }
+    });
+    if (!pointer.classList.contains('active')) {
+      pointer.classList.add('active');
+    }
+    slides.forEach((slide, i) => {
+      index === i
+        ? (slide.style.display = 'block')
+        : (slide.style.display = 'none');
+    });
+  };
+});
